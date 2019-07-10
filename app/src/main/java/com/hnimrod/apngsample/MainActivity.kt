4176d4d5adf1b01fc34dc18afe51454c7afe037d
h-nimrod <h.tanabe@gmail.com>
2019-07-11 00:12:36 +0900

    first commit

---
+++ app/src/main/java/com/hnimrod/apngsample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hnimrod.apngsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hnimrod.apngsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+}
