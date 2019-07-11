336c35a3c51c04a494cb0b7c7117db9edb24cad2
h-nimrod <h.tanabe@gmail.com>
2019-07-12 01:22:23 +0900

    implement sample apng loading

--- app/src/main/java/com/hnimrod/apngsample/MainActivity.kt
+++ app/src/main/java/com/hnimrod/apngsample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.hnimrod.apngsample.databinding.ActivityMainBinding
+import com.linecorp.apng.ApngDrawable
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadDefaultAPNG()
+    }
+
+    private fun loadDefaultAPNG() {
+        ApngDrawable.decode(resources, R.drawable.apng).apply {
+            setTargetDensity(resources.displayMetrics)
+            binding.imageView.setImageDrawable(this)
+            start()
+        }
     }
 
 }
